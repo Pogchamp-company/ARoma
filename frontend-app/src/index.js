8275dfe6695ddca5a9d8d7f9e9605522652223eb
AlexandrovRoman <AlexandrovRomanisi@ya.ru>
2021-07-09 16:54:52 +0400

    Create scss compiling

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -1,3 +1,4 @@
+import "./scss/style.scss"
 import App from "./components/App";
 import React from "react";
 import {render} from "react-dom";
--- frontend-app/webpack.config.js
+++ frontend-app/webpack.config.js
@@ -1,14 +1,25 @@
 const path = require("path");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "./static/js"),
+        path: path.resolve(__dirname, "./static/bundles"),
         filename: "[name].js",
     },
     module: {
         rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader?url=false",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -27,6 +38,9 @@ module.exports = {
                 // This has effect on the react lib size
                 NODE_ENV: JSON.stringify("production"),
             },
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
         }),
     ],
 };
