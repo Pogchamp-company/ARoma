6e3ba237225e9ae6fa44d94000a607d631e28090
AlexandrovRoman <AlexandrovRomanisi@ya.ru>
2021-07-30 11:41:48 +0400

    Filter products by attributes

--- backend-app/handlers/products.go
+++ backend-app/handlers/products.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"aroma/models"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"strconv"
 )
 
@@ -15,6 +17,26 @@ func GetProduct(context *gin.Context) {
 	})
 }
 
+func filterProductsByAttributes(query *gorm.DB, filters map[string]interface{}) {
+	if price, ok := filters["price"]; ok {
+		query.Where("price >= ?", price.(map[string]interface{})["Min"])
+		query.Where("price <= ?", price.(map[string]interface{})["Max"])
+	}
+	if attributes, ok := filters["attributes"]; ok {
+		attributes := attributes.([]interface{})
+		for _, attribute := range attributes {
+			attribute := attribute.(map[string]interface{})
+			switch attribute["Type"] {
+			case "string":
+				query.Where("attributes->>? = ?", attribute["Title"], attribute["Value"])
+			case "number":
+				query.Where("attributes->>? >= ?", attribute["Title"], attribute["Value"].(map[string]interface{})["Min"])
+				query.Where("attributes->>? <= ?", attribute["Title"], attribute["Value"].(map[string]interface{})["Max"])
+			}
+		}
+	}
+}
+
 func SearchProducts(context *gin.Context) {
 	productQuery := context.Request.URL.Query().Get("productQuery")
 	var products []models.Product
@@ -22,6 +44,12 @@ func SearchProducts(context *gin.Context) {
 	catalogId := context.Request.URL.Query().Get("catalogId")
 	if catalogId != "" {
 		query = query.Where("catalog_id = ?", catalogId)
+	}
+	rawFilters := context.Request.URL.Query().Get("filters")
+	var filters map[string]interface{}
+	err := json.Unmarshal([]byte(rawFilters), &filters)
+	if err == nil {
+		filterProductsByAttributes(query, filters)
 	}
 	query.Preload("Catalog").Find(&products)
 	context.JSON(200, gin.H{
